bc45c2182025ee0b8d7bc374870bdb1c1d6469fd
Adrian030304 <adrianromanenco1@gmail.com>
2024-04-28 21:58:43 +0300

    #

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ const form = document.querySelector(".form")
 console.log(form)
 const submitButton = document.querySelector("#submit-btn")
 
-submitButton.addEventListener("click",()=>{
-    form.submit()
-})
